1a6f7a0c3df4cbae10422dfb96e27a6b5e4c14b7
John Carlo <carlobase@gmail.com>
2020-04-18 19:27:02 -0500

    Updates

---
+++ precache-manifest.081574c7d0bf96748f562d29323addc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161985aa308c18e9528157c9aa811bdb",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "6cbfd6a7e5fd1d93e460",
+    "url": "/ClickyGame/static/css/main.5eceeac4.chunk.css"
+  },
+  {
+    "revision": "6317c9b25b6891613dd1",
+    "url": "/ClickyGame/static/js/2.242e5220.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ClickyGame/static/js/2.242e5220.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cbfd6a7e5fd1d93e460",
+    "url": "/ClickyGame/static/js/main.f3be2a1f.chunk.js"
+  },
+  {
+    "revision": "fe312f52d3a85951ce29",
+    "url": "/ClickyGame/static/js/runtime-main.0ae18fc5.js"
+  }
+]);
